85bdbd84ffba8aa35df88f6998bfdb627f3a5689
GOWTHAM BOYINA <161814106+Gowtham12234@users.noreply.github.com>
2025-06-16 10:45:43 +0530

    Time: 0 ms (100%), Space: 12.5 MB (7.76%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,12 +1,13 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
-        """
-        :type jewels: str
-        :type stones: str
-        :rtype: int
-        """
         count=0
+        # for i in stones:
+        #     if i in jewels:
+        #         count+=1
+        # return count
+
         for i in stones:
-            if i in jewels:
-                count+=1
+            for j in jewels:
+                if i==j:
+                    count+=1
         return count
