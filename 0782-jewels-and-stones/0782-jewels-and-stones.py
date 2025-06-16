424e4b8e10d52d7b2341b1e2c9389eb9566d40b2
GOWTHAM BOYINA <161814106+Gowtham12234@users.noreply.github.com>
2025-06-16 10:57:04 +0530

    Time: 4 ms (6.41%), Space: 12.6 MB (7.76%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
         count=0
+# approach-1
         # for i in stones:
         #     if i in jewels:
         #         count+=1
         # return count
-
+        
+# approach-2
         for i in stones:
             for j in jewels:
                 if i==j:
