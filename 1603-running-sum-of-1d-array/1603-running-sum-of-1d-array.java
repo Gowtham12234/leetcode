57ce06ff5e56504bf2abbbc88a472795fa931387
GOWTHAM BOYINA <161814106+Gowtham12234@users.noreply.github.com>
2025-06-27 22:47:25 +0530

    Time: 0 ms (100%), Space: 42.8 MB (39.82%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int [] prefixsum=new int [nums.length];
-        prefixsum[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-            prefixsum[i]=prefixsum[i-1]+ nums[i];
+        int n=nums.length;
+        int[] prefix=new int[n];
+        prefix[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i];
         }
-        return prefixsum;
-
-        
+        return prefix;
     }
 }
