d5f9517e68f5c4ae14c2250016c14f4705999d88
GOWTHAM BOYINA <161814106+Gowtham12234@users.noreply.github.com>
2025-08-24 16:39:42 +0530

    Time: 2 ms (83.56%), Space: 46 MB (31.4%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
+        int maxlen=0;
+        for(int i=0;i<n;i++){
+            if(i>maxlen){
+                return false;
+            }
+            maxlen=Math.max(i+nums[i],maxlen);
+        }
+        return true;
+    }
+}
