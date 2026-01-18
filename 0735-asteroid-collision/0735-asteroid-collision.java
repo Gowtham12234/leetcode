d6e5ecad7ddea18bc7030ec5687288012858a9ec
GOWTHAM BOYINA <161814106+Gowtham12234@users.noreply.github.com>
2026-01-18 23:15:55 +0530

    Time: 5 ms (64.66%), Space: 47.1 MB (49.29%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,25 +1,23 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        int n=asteroids.length;
-        List<Integer> st=new ArrayList<>();
-        for(int i=0;i<n;i++){
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<asteroids.length;i++){
             if(asteroids[i]>0){
-                st.add(asteroids[i]);
-            }
-            else{
-                while(!st.isEmpty() && st.get(st.size()-1) >0 && st.get(st.size() -1)<Math.abs(asteroids[i])){
-                    st.remove(st.size()-1);
+                st.push(asteroids[i]);
+            }else{
+                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(asteroids[i])){
+                    st.pop();
                 }
-                if(!st.isEmpty() && st.get(st.size()-1) == Math.abs(asteroids[i])){
-                    st.remove(st.size()-1);
+                if(!st.isEmpty() && st.peek() == Math.abs(asteroids[i])){
+                    st.pop();
                 }
-                else if(st.isEmpty() || st.get(st.size()-1)<0){
-                    st.add(asteroids[i]);
+                else if(st.isEmpty() || st.peek() < 0){
+                    st.push(asteroids[i]);
                 }
             }
         }
         int[] result=new int[st.size()];
-        for(int i=0;i<st.size();i++){
+        for(int i=0;i<st.size() ;i++){
             result[i]=st.get(i);
         }
         return result;
