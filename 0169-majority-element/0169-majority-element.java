5c2edbfd56159561cf16d1148148202c4a6faccc
GOWTHAM BOYINA <161814106+Gowtham12234@users.noreply.github.com>
2026-01-19 22:54:06 +0530

    Time: 13 ms (30.07%), Space: 52.6 MB (82.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        
+        int n=nums.length/2;
         HashMap<Integer,Integer> hmap=new HashMap<>();
-        for(int num:nums){
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
             hmap.put(num,hmap.getOrDefault(num,0)+1);
-            
         }
         for(int num:hmap.keySet()){
-            if(hmap.get(num)>(nums.length/2)){
+            if(hmap.get(num)>n){
                 return num;
             }
         }
-        return -1;
+        return 0;
     }
 }
